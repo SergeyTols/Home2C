bb15ddbe81662f98086527c74327af01f573d750
Sarge <sarge2033@gmail.com>
2022-08-10 20:49:57 +0300

    Первое задание

---
+++ Task1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+
+Console.Write("Введите трёхзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if(num > 99 & num < 1000)
+{
+    int Number = (num % 100) / 10;
+    Console.WriteLine("Вторая цифра " +Number + ".");
+}
+else
+{
+    Console.WriteLine("Введите ТРЁХЗНАЧНОЕ число. Попробуйте ещё раз.");
+}    
