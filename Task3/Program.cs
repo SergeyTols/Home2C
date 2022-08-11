7a3ec1cc1f6f80debcfb7d5ec9d2be22ba959b2c
Sarge <sarge2033@gmail.com>
2022-08-11 20:31:12 +0300

    исправления

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,7 +1,7 @@
 ﻿Console.Write("Введи цифру, обозначающую день недели: ");
 int num = Convert.ToInt32(Console.ReadLine());
 
-if(num >= 1 & num <= 7)
+if(num >= 1 && num <= 7)
 {
     if(num < 6)
     {
@@ -16,19 +16,3 @@ else
 {
     Console.Write("Сбегай к старшим, уточни, сколько дней в неделе!");
 }
-
-
-// Это интернет:
-// Вопрос есть... Я не понял как напечатать вертикальный /. 
-
-// // void CheckingTheDayOfTheWeek (int dayNumber) {
-// //   if (dayNumber == 6 || dayNumber == 7) {
-// //   Console.WriteLine("(этот день выходной) -> да");
-// //   }
-// //   else if (dayNumber < 1 || dayNumber > 7) {
-// //     Console.WriteLine("это вообще не день недели");
-// //   }
-// //   else Console.WriteLine("(этот день не выходной) -> нет");
-// }
-
-
