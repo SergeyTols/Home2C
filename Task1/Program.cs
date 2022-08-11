e2039dd6c04dd61a8216d6730ba19ed56ba4c68f
Sarge <sarge2033@gmail.com>
2022-08-11 20:33:39 +0300

    исправления 2

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -3,7 +3,7 @@
 Console.Write("Введите трёхзначное число: ");
 int num = Convert.ToInt32(Console.ReadLine());
 
-if(num > 99 & num < 1000)
+if(num > 99 && num < 1000)
 {
     int Number = (num % 100) / 10;
     Console.WriteLine("Вторая цифра " +Number + ".");
